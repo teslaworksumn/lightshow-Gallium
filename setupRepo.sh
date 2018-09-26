5d88729e5f55b72483f910ec6d0b3565d221e5c0
theeldestelder <joshuag98@gmail.com>
2018-09-25 23:20:10 -0500

    Move shell script to Node.js and update README.md
    The conversion of a shell script to a Node.js script is to make sure
    that everything can be run on all operating systems.  The .gitignore was
    updated to reflect the new commands needed to be run.  More links and
    some grammar/formatting changes was also added to the README.

--- setupRepo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eu
-REPO_ROOT=$(cd "`dirname "$0"`" && pwd)
-PARENT_DIR=$(cd "$REPO_ROOT/.." && pwd)
-
-rm "-rf" "${REPO_ROOT}/build"
-rm "-rf" "${REPO_ROOT}/app/shows"
-
-mkdir "${REPO_ROOT}/build"
-mkdir "${REPO_ROOT}/app/shows"
