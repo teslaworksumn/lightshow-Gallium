221655d5bbe05dd0767617804c0d51bcc52157c6
Ryan Fredlund <rfredlund13@gmail.com>
2018-11-05 19:28:37 -0600

    Passing linter

--- app/js/dmx.js
+++ app/js/dmx.js
@@ -11,6 +11,19 @@ const SERIAL_PORT = '/dev/ttyUSB0';
 // Create a universe
 const universe = dmx.addUniverse('demo', DRIVER, SERIAL_PORT);
 
+// Sets a range of channels to be on or off. Range is inclusive on both ends
+// channelStart is an int, channelEnd is an int, value is an int in range [0, 255]
+// Channels are 0-indexed
+function setRange(channelStart, channelEnd, value) {
+    const channels = {};
+    for (let i = channelStart; i <= channelEnd; i += 1) {
+        // DMX is 1-indexed. This should be the only place where the +1 is added
+        channels[i + 1] = value;
+    }
+
+    universe.update(channels);
+}
+
 // Sets a DMX channel to be on or off
 // Channel is an int, value is an int in range [0, 255]
 // Channels are 0-indexed
@@ -18,26 +31,9 @@ function setChannel(channel, value) {
     setRange(channel, channel, value);
 }
 
-// Sets a range of channels to be on or off. Range is inclusive on both ends
-// channelStart is an int, channelEnd is an int, value is an int in range [0, 255]
-// Channels are 0-indexed
-function setRange(channelStart, channelEnd, value) {
-    console.log("Setting DMX channels in range [" + (channelStart+1) + ", " + (channelEnd+1) + "] to " + value);
-
-    let channels = {};
-    for (var i = channelStart; i <= channelEnd; i++) {
-        // DMX is 1-indexed. This should be the only place where the +1 is added
-        channels[i+1] = value;
-    }
-    
-    universe.update(channels);
-}
-
 // Sets all channels to be on or off
 // value is an int in range [0, 255]
 function setAll(value) {
-    console.log("Setting all channels to " + value);
-
     universe.updateAll(value);
 }
 
