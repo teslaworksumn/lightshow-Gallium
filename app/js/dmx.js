1130c65d1cc19c5c90b3ffca0a6ad35c0b5e1436
Ryan Fredlund <rfredlund13@gmail.com>
2018-11-08 08:36:43 -0600

    Separated page setup js from outputting js. Passing lint

--- app/js/dmx.js
+++ app/js/dmx.js
@@ -1,42 +1,122 @@
-const parent = window.parent;
 
-const fs = parent.require('fs');
-const DMX = parent.require('dmx');
 
-const dmx = new DMX();
+/* Fading setup */
+let isFading = false;
+let currentFade = 0;
+const fadeDuration = 5000;
+const fadeInterval = 50;
+let fadeChange = 255.0 * fadeInterval / fadeDuration;
 
-const DRIVER = 'enttec-usb-dmx-pro';
-const SERIAL_PORT = '/dev/ttyUSB0';
+// Returns the value for the element with the given id, parsed as an int.
+// If failed to parse or find, returns default value
+function getIntFromElementById(id, defaultVal) {
+    const val = parseInt(document.getElementById(id).value, 10);
 
-// Create a universe
-const universe = dmx.addUniverse('demo', DRIVER, SERIAL_PORT);
-
-// Sets a range of channels to be on or off. Range is inclusive on both ends
-// channelStart is an int, channelEnd is an int, value is an int in range [0, 255]
-// Channels are 0-indexed
-function setRange(channelStart, channelEnd, value) {
-    const channels = {};
-    for (let i = channelStart; i <= channelEnd; i += 1) {
-        // DMX is 1-indexed. This should be the only place where the +1 is added
-        channels[i + 1] = value;
+    if (Number.isNaN(val)) {
+        alert(`Hey, ${val} isn't an int! (${id})`);
+        return defaultVal;
     }
 
-    universe.update(channels);
+    return val;
 }
 
-// Sets a DMX channel to be on or off
-// Channel is an int, value is an int in range [0, 255]
-// Channels are 0-indexed
-function setChannel(channel, value) {
-    setRange(channel, channel, value);
+// Updates the current fade amount. Only changes if isFading is set
+function updateFade() {
+    if (isFading) {
+        currentFade += fadeChange;
+
+        if (currentFade < 0) {
+            currentFade = 0;
+            fadeChange = -fadeChange;
+        }
+        if (currentFade > 255) {
+            currentFade = 255;
+            fadeChange = -fadeChange;
+        }
+
+        // Divide by 1 to make it an int
+        setAll(parseInt(currentFade, 10));
+    }
 }
 
-// Sets all channels to be on or off
-// value is an int in range [0, 255]
-function setAll(value) {
-    universe.updateAll(value);
+// Sets up all of the button.onclick methods for the DMX page
+function setupButtons() {
+    /* Single channels */
+
+    document.getElementById('channelOn').onclick = function channelOn() {
+        const channel = getIntFromElementById('singleChannel', -1);
+        if (channel !== -1) {
+            setChannel(channel, 255);
+        }
+    };
+    document.getElementById('channelOff').onclick = function channelOff() {
+        const channel = getIntFromElementById('singleChannel', -1);
+        if (channel !== -1) {
+            setChannel(channel, 0);
+        }
+    };
+
+    /* Range of channels */
+
+    document.getElementById('rangeOn').onclick = function rangeOn() {
+        const start = getIntFromElementById('rangeStart', -1);
+        if (start === -1) return;
+
+        const end = getIntFromElementById('rangeEnd', -1);
+        if (end === -1) return;
+
+        setRange(start, end, 255);
+    };
+    document.getElementById('rangeOff').onclick = function rangeOff() {
+        const start = getIntFromElementById('rangeStart', -1);
+        if (start === -1) return;
+
+        const end = getIntFromElementById('rangeEnd', -1);
+        if (end === -1) return;
+
+        setRange(start, end, 0);
+    };
+
+    /* Box */
+
+    document.getElementById('boxOn').onclick = function boxOn() {
+        const boxIdx = getIntFromElementById('boxNumber', -1);
+        if (boxIdx === -1) return;
+
+        const startIdx = boxIdx * 16;
+        const endIdx = startIdx + 15;
+        setRange(startIdx, endIdx, 255);
+    };
+
+    document.getElementById('boxOff').onclick = function boxOff() {
+        const boxIdx = getIntFromElementById('boxNumber', -1);
+        if (boxIdx === -1) return;
+
+        const startIdx = boxIdx * 16;
+        const endIdx = startIdx + 15;
+        setRange(startIdx, endIdx, 0);
+    };
+
+    /* All channels */
+
+    document.getElementById('allOn').onclick = function allOn() { setAll(255); };
+    document.getElementById('allOff').onclick = function allOff() { setAll(0); };
+    document.getElementById('fadeOn').onclick = function fadeOn() {
+        // Reset and enable fade
+        currentFade = 0;
+        isFading = true;
+    };
+    document.getElementById('fadeOff').onclick = function fadeOff() {
+        // Reset and disable fade
+        currentFade = 0;
+        isFading = false;
+    };
 }
 
+// Sets up the page
+function setupDmxPage() {
+    setInterval(updateFade, fadeInterval);
+    setupButtons();
+}
 
-// Run some command. Send to max out all channels
-// fs.chmodSync(SERIAL_PORT, 666);
+setupDmxPage();
