f058f30587c5c3daea9967bcc94b06eee4df6a42
DavidThe4sian <davidlzma@gmail.com>
2018-11-13 23:48:11 -0600

    Revert "Revert "Add Audio File""
    This reverts commit 185ebbe0e02b09744d7d43005b60faaa18fa5e4a.
    
    reverted reverting my commit - accidentally did some git shennanigans

--- app/js/show.js
+++ app/js/show.js
@@ -1,4 +1,6 @@
 const table = document.getElementById('tableBody');
+const { dialog } = window.parent.require('electron').remote;
+const addButton = document.getElementById('addShow');
 
 // the directory of the current show is contained in the iframe.value attribute
 const showDir = window.parent.document.getElementById('frame').value;
@@ -7,7 +9,22 @@ const showDataPath = path.join(showDir, 'show.json');
 // the json file containing the playlist of the show
 const showData = JSON.parse(fse.readFileSync(showDataPath));
 
-const playlistElements = showData.Playlist;
+
+//function to add a song
+addButton.onclick = function addSong() {
+  dialog.showOpenDialog({
+    title: 'Choose an audio file',
+    filters: [
+      {name: 'Audio', extensions: ['mp3', 'wav', 'flac', 'ogg']},
+      {name: 'All', extensions: ['*']},
+    ],
+  }, (filenames) => {
+    if(filenames !== undefined) {
+      playlistElements.push(filenames[0]);
+      fse.writeFileSync(showDataPath, JSON.stringify(showData,null, 2))
+    }
+  });
+}
 
 // push all playlist items (and associated audio if applicable)
 // names onto table to be rendered for each show
