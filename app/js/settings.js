71f1214f4166676551e1519ec42c0a656f8bb661
Ryan Fredlund <rfredlund13@gmail.com>
2018-11-16 16:01:15 -0600

    Moved html-specific stuff from settings.js to settings.html

--- app/js/settings.js
+++ app/js/settings.js
@@ -1,9 +1,3 @@
-const parent = window.parent;
-
-const fse = parent.require('fs-extra');
-const path = parent.require('path');
-const serialport = parent.require('serialport');
-
 /* Functions that are not class-specific */
 
 // Filters whether the device with the given location and manufacturer is indeed a DMX device
@@ -28,9 +22,14 @@ function sameDevice(devA, devB) {
 
 class Settings {
     constructor(onLoad) {
-        this.settingsConfigPath = path.resolve('app/config/settings.json');
-        this.dmxSelection = document.getElementById('dmxDeviceSelection');
+        const parent = window.parent;
+        const path = parent.require('path');
+        
+        this.fse = parent.require('fs-extra');
+        this.serialport = parent.require('serialport');
 
+        this.settingsConfigPath = path.resolve('app/config/settings.json');
+        
         this.dmxDevices = [];
         this.selectedDevice = 0;
         this.settingsConfig = null;
@@ -51,66 +50,43 @@ class Settings {
         return this.dmxDevices[this.selectedDevice];
     }
 
-    // Handles when the <select> for the dmx devices has its value change
-    dmxDeviceOnChange() {
-        this.selectedDevice = parseInt(this.dmxSelection.value, 10);
+    // Gets all discovered DMX devices
+    getDmxDevices() {
+        return this.dmxDevices;
+    }
 
-        if (Number.isNaN(this.selectedDevice)) {
-            alert(`Selected device '${this.selectedDevice}' somehow not a number!`);
-            this.selectedDevice = 0;
+    // Selects the device with the given index
+    // Returns null if out of range
+    // This updates future calls to getCurrentDmxDevice to get the returned device
+    selectDevice(index) {
+        if (index >= this.dmxDevices.length || index < 0) {
+            return null;
         }
 
-        // Save change to file
+        this.selectedDevice = index;
+
+        // Since we changed the selected device, save
         this.save();
-    }
 
-    // Resets the HTML elements relating to the DMX selection
-    // Also resets the related dmxDevices array
-    resetDmxHtml() {
-        // Array must match the DOM
-        this.dmxDevices = [];
-
-        // Remove all options from combo box
-        while (this.dmxSelection.firstChild) {
-            this.dmxSelection.removeChild(this.dmxSelection.firstChild);
-        }
-    }
-
-    // Adds an option to the DMX device selection, linking to the devices[] array through the index
-    addDeviceToHTML(index, textValue) {
-        if (!this.dmxSelection) return; // Not on Settings page (required)
-
-        // Create new element
-        const newOption = document.createElement('option');
-        newOption.value = String(index);
-        newOption.innerHTML = textValue;
-
-        // Add to selection box
-        this.dmxSelection.appendChild(newOption);
+        return getCurrentDmxDevice();
     }
 
     // Adds a DMX device. Can be done through polling devices or set through loading settings.
     addDmxDevice(location, manufacturer, portIdx) {
         // Add to devices array
         this.dmxDevices.push({
+            index: portIdx,
             location,
             manufacturer,
         });
-
-        // Add reference to HTML
-        this.addDeviceToHTML(portIdx, `${location} (${manufacturer})`);
     }
 
     // Updates the devices array with all detected DMX serial devices
-    getDmxDevices() {
+    refresh() {
         // See https://serialport.io/docs/api-stream#serialportlist
-        serialport.list()
+        this.serialport.list()
             .then((ports) => {
                 let portIdx = 0;
-                const oldSelectedDevice = this.getCurrentDmxDevice();
-
-                // Reset the html stuff
-                this.resetDmxHtml();
 
                 // Check each port to see if it matches our expected device
                 ports.forEach((port) => {
@@ -123,16 +99,6 @@ class Settings {
                         portIdx += 1;
                     }
                 });
-
-                // We updated our list. Try to keep selecting the current device
-                if (oldSelectedDevice !== null) {
-                    for (let i = 0; i < this.dmxDevices; i += 1) {
-                        if (this.sameDevice(this.dmxDevices[i], oldSelectedDevice)) {
-                            this.dmxSelection.selectedIndex = i;
-                            break;
-                        }
-                    }
-                }
 
                 // We updated, so save
                 this.save();
@@ -156,24 +122,16 @@ class Settings {
         };
 
         // Write to file
-        fse.writeFileSync(this.settingsConfigPath, JSON.stringify(this.settingsConfig, null, 2));
+        this.fse.writeFileSync(this.settingsConfigPath, JSON.stringify(this.settingsConfig, null, 2));
     }
 
     // Loads settings from file
     load(onLoad) {
         // Make sure config file exists before using
-        fse.pathExists(this.settingsConfigPath).then((exists) => {
+        this.fse.pathExists(this.settingsConfigPath).then((exists) => {
             if (exists) {
                 // Get settings
-                this.settingsConfig = JSON.parse(fse.readFileSync(this.settingsConfigPath));
-
-                // Set HTML elements
-                const currentDmxDevice = JSON.parse(this.settingsConfig.dmxDevice);
-                if (currentDmxDevice !== null) {
-                    const loc = currentDmxDevice.location;
-                    const man = currentDmxDevice.manufacturer;
-                    this.addDmxDevice(loc, man, 0);
-                }
+                this.settingsConfig = JSON.parse(this.fse.readFileSync(this.settingsConfigPath));
             } else {
                 // File doesn't exist, so create an empty config file there (save with defaults)
                 this.save();
@@ -187,41 +145,10 @@ class Settings {
     }
 }
 
-/* Manage settings being shared across multiple pages */
-
-// Set up global settings here
-let currentSettings = new Settings();
-const settingsChangedObservers = [];
-
-// Adds an observer for when settings are updated.
-// The listener should be a function that takes one argument, the new settings.
-function addSettingsChangedObserver(listener) {
-    settingsChangedObservers.push(listener);
-}
-
-// Called when any setting is updated. Updates the global settings and notifies all observers.
-function settingsChanged(newSettings) {
-    // Validate the updated settings
-    if (newSettings === null) return;
-
-    // Update settings
-    currentSettings = newSettings;
-
-    // Notify observers
-    for (let i = 0; i < settingsChangedObservers.length; i += 1) {
-        settingsChangedObservers[i](currentSettings);
-    }
-}
-
 // If this is being run in the browser, module isn't defined.
 // We want the module.exports though for other pieces to get the settings.
 if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = {
-        currentSettings,
-        addSettingsChangedObserver,
-    };
+    module.exports = Settings;
 } else {
-    window.currentSettings = currentSettings;
-    window.addSettingsChangedObserver = addSettingsChangedObserver;
-    currentSettings.getDmxDevices();
+    window.Settings = Settings;
 }
