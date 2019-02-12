57f211f60ce02b2b7eb7375e9a11dc003dd4805d
theeldestelder <joshuag98@gmail.com>
2019-02-12 02:12:10 -0600

    Begin channel mapping implementation
    * Add radio buttons to select automatic or manual channel mapping
    * Add UI divs corresponding to either both automatic and manual channel
    mapping
    * Include text prompts for each type of mapping
    * Add dictionary containing mapping of MIDI keyboard keys to DMX channels
    * Only show the div corresponding to the selected channel mapping mode
    * Ensure that the number of DMX channels is set and that only numbers are
    entered as empty channels
    * Populate mapping dictionary with valid DMX channels for every key
    * Ensure that the keysDown object is looking at the correct keys and DMX
    channels

--- app/js/midiTest.js
+++ app/js/midiTest.js
@@ -1,15 +1,19 @@
 var navigator = parent.require('web-midi-api');
 const DMX = parent.require('dmx');
+const fs = parent.require('fs');
 
 const KEY_PRESSED = "90";
 const KEY_RELEASED = "80";
+
+const INVALID = -1;
+const NUM_KEYS = 12; // keys in an octave
 
 var midi;
 var inputs;
 var outputs;
 var midiPort;
 
-var numChannels = 0;
+var numChannels = INVALID;
 var keysDown = {};
 
 const select = document.getElementById("midiSelect");
@@ -17,6 +21,11 @@ const refresh = document.getElementById("midiRefresh");
 const setMidi = document.getElementById("midiSet");
 const midiName = document.getElementById("selectedMidiController");
 const setChannelCount = document.getElementById("setChannelCount");
+
+// dictionary containing channel mappings
+// key: keyboard note
+// value: array of DMX channel numbers
+var mapping = {};
 
 refresh.onclick = function refreshMidiDevices() {
     navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
@@ -33,22 +42,105 @@ setMidi.onclick = function setMidiDevice() {
     });
 }
 
+// show the relevant portion of the UI
+function showMappingInterface(choice) {
+    const autoDiv = document.getElementById("mapAuto");
+    const manualDiv = document.getElementById("mapManual");
+
+    if (choice.value === "automatic") {
+        autoDiv.setAttribute("style", "display: block");
+        manualDiv.setAttribute("style", "display: none");
+    } else if (choice.value === "manual") {
+        manualDiv.setAttribute("style", "display: block");
+        autoDiv.setAttribute("style", "display: none");
+    }
+}
+
+// Maps every key to 1 DMX channel
+// DMX channels may repeat so that every key has exactly 1 channel of DMX output
+function mapChannelsAuto() {
+    let emptyChannels = document.getElementById("autoEmptyChannels").value;
+    const label = document.getElementById("autoErrorText");
+    emptyChannels = emptyChannels.split(/,/);
+
+    var validChannels = [];
+
+    for (let i = 0; i < emptyChannels.length; i++) {
+        if (!isNumber(emptyChannels[i])) {
+            label.innerText = "Invalid number entered.  Ensure only integers and commas are entered"
+
+            return;
+        }
+
+        emptyChannels[i] = parseInt(emptyChannels[i], 10);
+    }
+
+    label.innerText = "";
+
+    if (numChannels === INVALID) {
+        label.innerText = "Enter a valid number of DMX channels and try again"
+    } else {
+        // create an array consisting of the lowest channel numbers that haven't been excluded
+        // need numChannels number of valid channels
+        let totalChannels = 0;
+        let currentChannelNum = 1; // DMX is 1-indexed
+        while (totalChannels < numChannels) {
+            if (emptyChannels.includes(currentChannelNum)) {
+                currentChannelNum++;
+            } else {
+                validChannels.push(currentChannelNum);
+                totalChannels++;
+                currentChannelNum++;
+            }
+        }
+
+        // add the DMX channel for a key to a mapping
+        // DMX channels can repeat
+        for (let i = 0; i < NUM_KEYS; i++) {
+            mapping[i] = validChannels[i % numChannels];
+        }
+    }
+
+    // ensure the object counting each key down keeps track of the channels that are mapped
+    for (let i = 0; i < validChannels.length; i++) {
+        keysDown[validChannels[i]] = 0;
+    }
+}
+
+function mapChannelsManual() {
+    
+}
+
+function isNumber(value) {
+    value = parseInt(value, 10);
+    if (Number.isNaN(value) || value !== Number(value)) {
+        return false;
+    }
+
+    return true;
+}
+
 
 setChannelCount.onclick = function setNumChannels() {
     numChannels = document.getElementById("numChannels").value;
+    numChannels = parseInt(numChannels, 10);
     const channelInt = parseInt(numChannels, 10);
 
     // ensures the value entered is a number
-    if (Number.isNaN(channelInt) || channelInt !== Number(channelInt)) {
+    if (!isNumber(channelInt)) {
+    //if (Number.isNaN(channelInt) || channelInt !== Number(channelInt)) {
         document.getElementById("textNumChannels").innerText = "invalid number";
+        numChannels = INVALID;
 
         // ensures no keys lights can be pressed when an improper DMX channel
         // count is inputted
-        keysDown = {};
+        //keysDown = {};
     } else {
+        /*
         for (let i = 1; i <= numChannels; i++) {
             keysDown[i] = 0;
         }
+        */
 
         document.getElementById("textNumChannels").innerText = channelInt;
     }
@@ -112,11 +204,12 @@ function onMidiIn(ev) {
 // takes MIDI input data and turns on lights accordingly
 function midiToDmx(data) {
     const action = data[0].toString(16);
-    const key = data[1];
+    const key = data[1] % NUM_KEYS; // returns number from 0-11 to index into the mapping dictionary
     const brightness = data[2] * 2; // multiply by 2 to get 0-255 values
 
-    // DMX channels are 1 indexed so 1 needs to be added
-    const channel = (key % parseInt(numChannels)) + 1;
+    // mapping is looked up and returned based on previous mapping calculation
+    const channel = mapping[key];
+
 
     const channelData = {};
     channelData[channel] = brightness;
@@ -125,6 +218,7 @@ function midiToDmx(data) {
     if (!isEmpty(keysDown)) {
         // key is pressed
         if (action == KEY_PRESSED) {
+            console.log(key);
             parent.universe.update(channelData);
             keysDown[channel] += 1;
         } else if (action == KEY_RELEASED) { // key is released
@@ -146,6 +240,10 @@ function isEmpty(obj) {
     }
 
     return true;
+}
+
+function createMapping(configFilePath) {
+    //fse
 }
 
 
