2080356eb083c0b8611e4552d849a3629fb05a0f
Zachary Guldberg <guldb016@umn.edu>
2020-11-20 00:24:04 -0600

    WIP Branch. Actually works

--- main.js
+++ main.js
@@ -16,6 +16,9 @@ function createWindow() {
         minHeight: 600,
         minWidth: 800,
         icon: path.join(__dirname, 'app/img/tesla_gear.png'),
+        webPreferences: {
+            nodeIntegration: true
+        }
     });
 
 
@@ -61,6 +64,8 @@ app.on('activate', () => {
     }
 });
 
+app.allowRendererProcessReuse = false;
+
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
-process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
+//process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = 'true';
